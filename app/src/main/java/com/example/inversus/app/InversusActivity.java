a9ab80c16fcc7691b77e3478335a15039f4e85d6
Kim Do Han <aksia3@naver.com>
2024-04-25 03:47:51 +0900

    버튼,스프라이트 생성자 수정.
    ....

--- app/src/main/java/com/example/inversus/app/InversusActivity.java
+++ app/src/main/java/com/example/inversus/app/InversusActivity.java
@@ -4,11 +4,14 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.example.inversus.framework.activity.GameActivity;
+import com.example.inversus.framework.view.Metrics;
 import com.example.inversus.game.MainScene;
 
 public class InversusActivity extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Metrics.setGameSize(16,0);
 
         super.onCreate(savedInstanceState);
 
---
+++ app/src/main/java/com/example/inversus/framework/interfaces/ITouchable.java
@@ -0,0 +1,8 @@
+package com.example.inversus.framework.interfaces;
+
+import android.view.MotionEvent;
+
+public interface ITouchable {
+    public boolean onTouchEvent(MotionEvent e);
+
+}

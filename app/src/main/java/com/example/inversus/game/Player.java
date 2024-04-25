75d4967eee03b43276e9074c1cfd68ab80d21746
Kim Do Han <aksia3@naver.com>
2024-04-25 13:15:21 +0900

    플레이어 총알 표시구현
    ...

--- app/src/main/java/com/example/inversus/game/Player.java
+++ app/src/main/java/com/example/inversus/game/Player.java
@@ -1,5 +1,8 @@
 package com.example.inversus.game;
 
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -10,23 +13,46 @@ import com.example.inversus.framework.view.Metrics;
 
 public class Player implements IGameObject {
 
-    private static final float PLAYERSIZE = 1;
-
+    private static final float PLAYERSIZE = 0.5f;
+    private static final int BULLETCOUNT = 5;
+    private static final float BULLETOFFSET = 0.3f;
+    private static final float BULLETSIZE = 0.1f;
     float x ,  y ;
     RectF DrawRect;
-    Paint  PlayerBodyColor;
-    float radius ;
-
+    Paint PlayerBodyColor;
+    Paint BulletColor;
+    float[] bulletPosX = new float[5];
+    float[] bulletPosY = new float[5];
     Player(){
         PlayerBodyColor = new Paint();
      PlayerBodyColor.setColor(Color.BLACK);
+        BulletColor = new Paint();
+        BulletColor.setColor(Color.RED);
+
      DrawRect = new RectF();
-     x  = 0 ;
-     y = 0 ;
-        radius = Math.min(PLAYERSIZE, PLAYERSIZE) / 2;
+     x  = 2 ;
+     y = 0;
+
+     for(int i = 0 ; i < BULLETCOUNT ;++i){
+
+         bulletPosX[i] = BULLETOFFSET * (float)(Math.cos(toRadians(360.f/BULLETCOUNT)*i));
+         bulletPosY[i] = BULLETOFFSET * (float)(Math.sin(toRadians(360.f/BULLETCOUNT)*i));
+
+     }
+
     }
     public void update(float elapsedSeconds){
         UpdateRect();
+        RotateBullet(elapsedSeconds);
+    }
+
+    public void RotateBullet(float elapsedSeconds){
+        for(int i = 0 ; i < BULLETCOUNT ;++i){
+            float X = bulletPosX[i];
+            float Y = bulletPosY[i];
+            bulletPosX[i] =  X * (float)Math.cos(elapsedSeconds) - Y * (float)Math.sin(elapsedSeconds);
+            bulletPosY[i] = X * (float)Math.sin(elapsedSeconds) + Y * (float)Math.cos(elapsedSeconds);
+        }
     }
     public void UpdateRect(){
 
@@ -38,7 +64,19 @@ public class Player implements IGameObject {
 
     }
     public void draw(Canvas canvas){
-    canvas.drawRect(DrawRect,PlayerBodyColor);
+    canvas.drawRoundRect(DrawRect,0.1f,0.1f,PlayerBodyColor);
+
+
+        for(int i = 0 ; i < BULLETCOUNT ;++i){
+
+           RectF drawR = new RectF(  bulletPosX[i]- BULLETSIZE +x,
+                   bulletPosY[i]+ BULLETSIZE+y,
+                   bulletPosX[i]+ BULLETSIZE+x,
+                   bulletPosY[i]- BULLETSIZE +y);
+
+           canvas.drawOval(drawR,BulletColor);
+
+        }
 
     }
 
